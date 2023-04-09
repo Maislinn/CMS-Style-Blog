cb2814d67fb895c94d9affcbf2a9c8429c294279
Maislinn <maislinn.helfer@gmail.com>
2023-04-09 11:01:56 -0700

    adding utils

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const flash = require('connect-flash');
-const path = require('path');
-const { engine } = require('express-handlebars');
+//const path = require('path');
+//const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const db = require('./config/database');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
-const cors = require('cors');
+//const cors = require('cors');
+const helpers = require('./utils/helpers')
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // using cors to allow cross origin resource sharing
-app.use(cors());
+//app.use(cors());
 
 // middleware
 app.use(express.json());
@@ -44,6 +48,7 @@ app.use (
 require('./config/passport.js');
 app.use(passport.initialize());
 app.use(passport.session());
+//app.use(session(sess));
 
 // flash middleware
 // used for storing messages https://www.npmjs.com/package/connect-flash
@@ -57,20 +62,32 @@ app.use((req, res, next) => {
     next();
 });
 
-// handlebars
+// // handlebars
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+// app.engine('handlebars', hbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+// //app.set('view engine', '.hbs');
+// //app.set("views", "./views");
 
-app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-//app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // routes
-app.use('/index', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
-app.use('/posts', require('./routes/posts.js'));
-app.use('/comments', require('./routes/comments.js'));
+// app.use('/index', require('./routes/index.js'));
+// app.use('/users', require('./routes/users.js'));
+// app.use('/posts', require('./routes/posts.js'));
+// app.use('/comments', require('./routes/comments.js'));
+app.use(routes);
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-});
+})
